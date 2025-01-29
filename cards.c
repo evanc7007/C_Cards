7225833bafb1423f0523f7f494476366f4e9345e
Evan <evanjasonchen@gmail.com>
2025-01-29 17:22:03 -0500

    simplified rank enum

--- cards.c
+++ cards.c
@@ -7,8 +7,8 @@
 //enum for suits
 typedef enum{SPADES, CLUBS, HEARTS, DIAMONDS} Suit;
 //enum for ranks
-typedef enum{ACE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6,
-    SEVEN = 7, EIGHT = 8, NINE = 9, TEN = 10, JACK = 11, QUEEN = 12, KING = 13} Rank;
+typedef enum{ACE = 1, TWO, THREE, FOUR, FIVE, SIX,
+    SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING} Rank;
 
 //struct for a card
 typedef struct{
